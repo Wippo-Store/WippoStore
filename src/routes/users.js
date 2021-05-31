81c154853d89f56b17b324725f32de82fa2c22fe
auroramc19 <auroramendezc.19@gmail.com>
2021-05-31 01:35:58 -0500

    avance usuario comprador x2

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,6 +4,10 @@ var router = express.Router();
 const pool = require('../db');
 
 /* GET users listing. */
+router.get('/principalUU', (req, res) => { /*UNREGISTERED USER*/
+    res.render('principalUU');
+});
+
 router.get('/principalC', (req, res) => {
     res.render('userC/principalC');
 });
@@ -12,4 +16,14 @@ router.get('/profileC', (req, res) => {
     res.render('userC/profileC');
 });
 
+router.get('/editProfileC', (req, res) => {
+    res.render('userC/editProfileC');
+});
+
+router.get('/addDirectionC', (req, res) => {
+    res.render('userC/addDirectionC');
+});
+router.get('/addCardC', (req, res) => {
+    res.render('userC/addCardC');
+});
 module.exports = router;
