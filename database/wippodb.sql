0c5968f0b73495704470957d34c0443e086f2a35
Rodrigo Rubio Haro <chavo0022009@gmail.com>
2021-06-06 13:37:59 -0500

    Se agregó la validación de Tokens

--- database/wippodb.sql
+++ database/wippodb.sql
@@ -34,8 +34,23 @@ BEGIN
     REPLACE INTO TokensCorreo(ID_Usuario, token, fecha) VALUES(ID_Usuario, token, now());
 END &&  
 DELIMITER ;  
-
 -- Example: call addToken("1234", 1);
+
+drop PROCEDURE if exists validateToken;
+DELIMITER &&  
+CREATE PROCEDURE validateToken (in user_token VARCHAR(40), in ID_Usuario_r int)  
+BEGIN
+    DECLARE tk Varchar(40);
+    SELECT `token` INTO tk FROM `TokensCorreo` WHERE ID_Usuario = ID_Usuario_r limit 1;
+    if(tk = user_token) then
+        DELETE FROM `TokensCorreo` WHERE ID_Usuario = ID_Usuario_r;
+        UPDATE Usuario  SET  Estatus = 'Activo'  WHERE ID_Usuario = ID_Usuario_r;
+    end if;
+END &&  
+DELIMITER ;  
+
+-- Example: call validateToken("1234", 1);
+
 
 create table if not exists Producto(
 	ID_Producto int(11) not null AUTO_INCREMENT,
--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -39,21 +39,47 @@ router.get('/recoverC', isNotLoggedIn, (req, res) => {
 
 router.get('/send', (req, res) => {
     token = 'wippo_token_' + Math.random().toString(36).substr(2, 9);
-    Host = req.get('Host');
-    link = "http://" + req.get('Host') + "/verify?id=wippo-" + token;
-    console.log("Sending email:")
-    // mails.sendValidation(link, "rodrigo.rubio.haro.digital@gmail.com");
-
-    // const { ID_Usuario } = req.body;
-    const ID_Usuario = 2;
+    const { ID_Usuario } = req.query;
+    const { mail } = req.query;
+    link = "http://" + req.get('Host') + "/authentication/verify?token=" + token + "&ID_Usuario=" + ID_Usuario;
+    console.log("Sending email:");
+    mails.sendValidation(link, mail);
     pool.query("CALL `addToken`(?, ?);", [token, ID_Usuario], (err, res) => {
         if (err) throw err;
-        console.log('Last insert ID:', res.insertId);
-        console.log("result:");
+        else {
+            console.log('Last insert ID:', res.insertId);
+            console.log("token addded");
+        }
     });
-
     res.redirect("/");
 })
+
+router.post('/verify', function (req, res) {
+    Host = "localhost:3000";
+    console.log(req.protocol + ":/" + req.get('Host'));
+    const { token } = req.query;
+    // console.log("token:" + token);
+    const { ID_Usuario } = req.query;
+    // console.log("ID_Usuario:" + ID_Usuario);
+    if ((req.protocol + "://" + req.get('Host')) == ("http://" + Host)) {
+        console.log("Domain is matched. Information is from Authentic email");
+        // console.log("Generating query: CALL `validateToken`(?, ?);" + [token, ID_Usuario]);
+
+        pool.query("CALL `validateToken`(?, ?);", [token, ID_Usuario], (err) => {
+            if (err) {
+                console.log("email is not verified");
+                res.end("<h1>Bad Request</h1>");
+                throw err;
+            } else {
+                console.log("email is verified");
+                res.send("<h1>Email is been Successfully verified");
+            }
+        });
+    }
+    else
+        res.end(`<h1>Request is from unknown source: ${req.protocol}://${req.get('Host')} == http://${Host}}`);
+
+});
 
 /* GET FORM */
 router.post('/signupC', passport.authenticate('local.signupC', { // signupC debe hacer passaporte?
