a788119c6a632db31ace6c367811b3b8a6785d3a
auroramc19 <auroramendezc.19@gmail.com>
2021-06-06 22:24:40 -0500

    pruebas

--- src/routes/products.js
+++ src/routes/products.js
@@ -6,13 +6,13 @@ const pool = require('../db');
 /* to open windows */
 router.get('/category', async(req, res) => {
     const products = await pool.query('SELECT * FROM producto');
-    res.render('product/category', { products, nombre: req.session.username, titulo: 'WippoStore' });
+    res.render('product/category', { products, user: req.session.user, titulo: 'WippoStore' });
 });
 
 router.get('/pDetails/:id', async(req, res) => {
     const idU = req.params.id;
     const products = await pool.query('SELECT * FROM producto WHERE ID_Producto = ?', [idU]);
-    res.render('product/pDetails', { products, nombre: req.session.username, titulo: 'WippoStore' });
+    res.render('product/pDetails', { products, user: req.session.user, titulo: 'WippoStore' });
 });
 
 module.exports = router;
