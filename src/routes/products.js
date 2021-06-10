5120a615db0046e88ca5c8db2e638bbbbbc96ec0
David Perez Antonio <david.perez.antonio51@gmail.com>
2021-06-10 16:11:40 -0500

    Corrección en la búsqueda por nombres
    Se realizó una corrección para poder realizar una búsqueda por nombres desde cualquier lado de la pagina web

--- src/routes/products.js
+++ src/routes/products.js
@@ -4,9 +4,24 @@ const router = express.Router();
 const pool = require('../db');
 
 /* to open windows */
+
 router.get('/category/:Product_Category', async(req, res) => {
     const products = await pool.query('SELECT * FROM producto where Categoria = ?', req.params.Product_Category);
     res.render('product/category', { products, user: req.session.user, titulo: 'WippoStore' });
+});
+
+router.get('/', async(req, res) => {
+    var Buscar = req.query.search;
+    var products;
+    const query_busqueda = "SELECT * FROM producto WHERE Nombre LIKE" + "'%" + Buscar + "%'";
+    if (req.query.search == '' || Buscar == undefined) {
+        products = await pool.query('SELECT * FROM producto');
+        console.log("Vacio");
+    } else {
+        products = await pool.query(query_busqueda);
+        console.log("Buscar" + Buscar);
+    }
+    res.render('product/Productos', { products, user: req.session.user, titulo: 'WippoStore' });
 });
 
 router.get('/pDetails/:id', async(req, res) => {
